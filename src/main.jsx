8ce611bd21592cdac9a1d362370c5ee967a73dc6
kritsanapongdj-ctrl <kritsanapong.dj@gmail.com>
2026-04-25 16:17:23 +0700

    Fix self-import loop in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// ถ้าคุณเซฟไฟล์โค้ดผมไว้ชื่อ VTrackApp.jsx ให้ใช้บรรทัดล่างนี้:
 import App from './VTrackApp.jsx' 
 import './index.css'
 
